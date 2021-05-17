13e7f1008a65cdf15602ac0ff16e4caafebbcd0c
robertscalzo <rscalzo@alumni.scu.edu>
2021-05-17 19:56:30 +0000

    Second lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,9 +2,16 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 public class WelcomeController {
+
+    @Value("${welcome.message}") String message;
+    public WelcomeController(String message){
+    this.message=message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
